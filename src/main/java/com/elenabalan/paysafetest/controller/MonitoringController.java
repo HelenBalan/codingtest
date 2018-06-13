962dd09c04bc895dfd7b55d51b4af2da1c1ed6e0
Helen Balan <helen.v.balan@gmail.com>
2018-06-13 08:56:21 -0400

    Working version

--- src/main/java/com/elenabalan/paysafetest/controller/MonitoringController.java
+++ src/main/java/com/elenabalan/paysafetest/controller/MonitoringController.java
@@ -1,6 +1,8 @@
 package com.elenabalan.paysafetest.controller;
 
 import com.elenabalan.paysafetest.model.MonitoringLogInfo;
+import com.elenabalan.paysafetest.model.StartQuery;
+import com.elenabalan.paysafetest.model.StopOrInfoQuery;
 import com.elenabalan.paysafetest.service.MonitoringService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,60 +21,13 @@ public class MonitoringController {
     }
 
     @RequestMapping(value = "/stop", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void stopMonitoring(@RequestBody StopQuery stopQuery) {
-        monitoringService.stop(stopQuery.getUri());
+    public void stopMonitoring(@RequestBody StopOrInfoQuery stopOrInfoQuery) {
+        monitoringService.stop(stopOrInfoQuery.getUri());
     }
 
     @RequestMapping(value = "/info", method = RequestMethod.PUT)
-    public MonitoringLogInfo getOverview(@RequestBody StopQuery stopQuery) {
-        return monitoringService.getOverview(stopQuery.getUri());
+    public MonitoringLogInfo getOverview(@RequestBody StopOrInfoQuery stopOrInfoQuery) {
+        return monitoringService.getOverview(stopOrInfoQuery.getUri());
     }
 }
 
-class StartQuery {
-    private String uri;
-    private double seconds;
-
-    public StartQuery(String uri, double seconds) {
-        this.uri = uri;
-        this.seconds = seconds;
-    }
-
-    public StartQuery() {
-    }
-
-    public String getUri() {
-        return uri;
-    }
-
-    public void setUri(String uri) {
-        this.uri = uri;
-    }
-
-    public double getSeconds() {
-        return seconds;
-    }
-
-    public void setSeconds(double seconds) {
-        this.seconds = seconds;
-    }
-}
-
-class StopQuery {
-    private String uri;
-
-    public StopQuery(String uri) {
-        this.uri = uri;
-    }
-
-    public StopQuery() {
-    }
-
-    public String getUri() {
-        return uri;
-    }
-
-    public void setUri(String uri) {
-        this.uri = uri;
-    }
-}
