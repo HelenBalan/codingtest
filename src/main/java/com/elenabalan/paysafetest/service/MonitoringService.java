03b2e2d9437e7bee6eccd00e640f9f7d49beef74
Helen Balan <helen.v.balan@gmail.com>
2018-06-13 12:13:27 -0400

    Working version, comments added

--- src/main/java/com/elenabalan/paysafetest/service/MonitoringService.java
+++ src/main/java/com/elenabalan/paysafetest/service/MonitoringService.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2018 Elena Balan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.elenabalan.paysafetest.service;
 
 import com.elenabalan.paysafetest.model.MonitoringFlag;
@@ -13,12 +30,23 @@ public class MonitoringService {
 
     private Map<String, MonitoringModel> monitoringData = new HashMap<>();
 
+    /**
+     *
+     * @param uri  watched server
+     * @return information about availability
+     */
+
     public MonitoringLogInfo getOverview(String uri) {
         MonitoringModel monitor = monitoringData.get(uri);
         if (monitor == null) return null;
-        return new MonitoringLogInfo(uri, monitor.getDuration().toMillis() / 1000, monitor.getPeriods());
+        return new MonitoringLogInfo(uri, monitor.getDuration().toMillis() / 1000.0, monitor.getPeriods());
     }
 
+    /**
+     *
+     * @param uri watched server
+     * @param seconds checking interval
+     */
     public synchronized void start(String uri, double seconds) {
         MonitoringModel monitor = monitoringData.get(uri);
         if (monitor != null) {
@@ -33,6 +61,10 @@ public class MonitoringService {
 
     }
 
+    /**
+     *
+     * @param uri watched server
+     */
     public synchronized void stop(String uri) {
 
         MonitoringModel monitor = monitoringData.get(uri);
